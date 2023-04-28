37e4a69bf4ad73face345bc45f061141b81a8ae4
Alexandre Cottin (Lx-Ctn) <design@lxctn.com>
2023-04-28 14:31:25 +0200

    fix: scale error in safari if zoom-in when switch orientation

--- src/utils/responsive.js
+++ src/utils/responsive.js
@@ -4,21 +4,25 @@ const MIN_HEIGHT_WITH_HEADER = 815; // Full content height
 const DEFAULT_SIZE = 2; // em
 
 export const handleResponsive = () => {
-	const isWidthTooSmall = window.innerWidth < MIN_WIDTH;
-	const isHeightTooSmall = window.innerHeight < MIN_HEIGHT_WITH_HEADER;
-	const isMinHeightTooSmall = window.innerHeight < MIN_HEIGHT;
+	// Safari modify the window size when zoom in : using document.documentElement.clientWidth instead of window.innerWidth
+	const appWidth = document.documentElement.clientWidth;
+	const appHeight = document.documentElement.clientHeight;
+
+	const isWidthTooSmall = appWidth < MIN_WIDTH;
+	const isHeightTooSmall = appHeight < MIN_HEIGHT_WITH_HEADER;
+	const isMinHeightTooSmall = appHeight < MIN_HEIGHT;
 
 	if (!isWidthTooSmall && !isHeightTooSmall) setFontRatio(1); // By default
 	else {
 		// If window width get smaller than the content, content scale down to fit the window :
-		const widthRatio = isWidthTooSmall ? window.innerWidth / MIN_WIDTH : 1;
+		const widthRatio = isWidthTooSmall ? appWidth / MIN_WIDTH : 1;
 
 		// If the scale get too small for good lisibility, we keep only the essential part (keypad and result display) to fit the screen :
 		const heightRatio = isMinHeightTooSmall
-			? window.innerHeight / MIN_HEIGHT
+			? appHeight / MIN_HEIGHT
 			: // If window height get smaller than the content, content scale down to fit the window :
 			isHeightTooSmall
-			? window.innerHeight / MIN_HEIGHT_WITH_HEADER
+			? appHeight / MIN_HEIGHT_WITH_HEADER
 			: 1;
 
 		// If width AND height get smaller, content scale down to the smallest :
