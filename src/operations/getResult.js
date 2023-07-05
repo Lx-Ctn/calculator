5369e992d5baa5cd478e5c85770f199796efdcc1
Alexandre Cottin (Lx-Ctn) <design@lxctn.com>
2023-07-05 15:45:44 +0200

    refactoring getResult function

--- src/operations/getResult.js
+++ src/operations/getResult.js
@@ -3,34 +3,39 @@ import { getFormattedNumber } from "./getFormattedNumber";
 export const getResult = (nextOperation, state) => {
 	const { current, result, oldValues } = state;
 
-	const isCurrentValue = current.value !== "" || result === null; // Last result act as current value if there is none.
+	const isCurrentValue = current.value !== "";
+	const isLastResult = result !== null;
+	//const isLastValue = lastCalculation.value !== "";
+
+	// For a new calculation, we just save the current value :
+	const isNewCalculation = !isLastResult && current.operation === "";
+	if (isNewCalculation) {
+		const currentValue = isCurrentValue ? current.value : 0;
+		return {
+			result: null,
+			oldValues: [{ ...getFormattedNumber(currentValue), operation: "", result: null }, ...oldValues],
+		};
+	}
 
 	// If there is no current value entered, we repeat last operation :
-	const old = oldValues[0] ?? { value: "", display: 0, result: null, operation: "" };
-	const prevValue = result === null ? +old.value : +result.value;
-	const oldOperation = nextOperation === "=" ? old.operation : nextOperation;
-	const operation = isCurrentValue ? current.operation : oldOperation;
-	//const operation = current.operation && current.operation !== "=" ? current.operation : oldOperation;
-	const currentValue = isCurrentValue ? +current.value : +old.value;
+	const lastCalculation = oldValues[0] ?? { value: "", display: 0, operation: "", result: null };
 
-	console.log({
-		operation,
-		//testOperation,
-		oldOperation,
-		nextOperation,
-		isCurrentValue,
-		currentOpe: current.operation,
-	});
+	const prevValue = isLastResult ? +result.value : +lastCalculation.value;
+	const currentValue = isCurrentValue ? +current.value : +lastCalculation.value;
+	const operation = isCurrentValue ? current.operation : lastCalculation.operation;
 
-	const total = operation ? getOperations[operation](prevValue, currentValue) : null;
-	const formattedTotal = total ? getFormattedNumber(total) : null;
+	let total;
+	const isValidOperation = operation && operation !== "=";
+	if (isValidOperation) total = getFormattedNumber(getOperations[operation](prevValue, currentValue));
+	else total = null;
 
-	const formattedCurrent = { ...current, display: getFormattedNumber(current.value).display }; // Get rid of extra "0" and ending "."
-	const dataToSave = isCurrentValue ? formattedCurrent : old;
+	const getFormattedCurrent = () => ({ ...getFormattedNumber(current.value), operation: current.operation }); // Get rid of extra "0" and ending "."
+	const dataToSave = isCurrentValue ? getFormattedCurrent() : { ...lastCalculation, operation };
 
+	console.log({ operation, prevValue, currentValue });
 	return {
-		result: formattedTotal,
-		oldValues: [{ ...dataToSave, result: formattedTotal }, ...oldValues],
+		result: total, // need to keep a separate result to be able to reset without erase history
+		oldValues: [{ ...dataToSave, result: total }, ...oldValues],
 	};
 };
 
@@ -39,5 +44,5 @@ const getOperations = {
 	"÷": (prev, cur) => prev / cur,
 	"−": (prev, cur) => prev - cur,
 	"+": (prev, cur) => prev + cur,
-	"=": () => null, // ca n'a aucun sens à vérifier
+	//"=": () => null, // ca n'a aucun sens à vérifier
 };
